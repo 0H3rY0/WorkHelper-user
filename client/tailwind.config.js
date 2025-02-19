691eb56264c8a13e65ca1464aa90e995072bf340
Szymon <szymers12345@gmail.com>
2025-02-19 23:51:45 +0100

    initial vite and react

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "custom-blue": "#3b4e63",
+        "custom-gray": "#575757",
+        "custom-blue-light": "#475d74",
+        "dark-gray": "#2d2d2d",
+      },
+    },
+  },
+  plugins: [],
+};
