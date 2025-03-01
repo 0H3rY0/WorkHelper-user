7d8a96e82a3bc509c3775a92a7f541554e2a5bed
Szymon <szymers12345@gmail.com>
2025-03-01 12:26:06 +0100

    addRaport endpoint modiefied tableName flexiblity

--- server/controllers/raport.controller.js
+++ server/controllers/raport.controller.js
@@ -3,50 +3,49 @@ require("dotenv").config();
 
 const DATABASE_NAME = process.env.DB_NAME;
 
-const addRaport = (req, res) => {
-  const { id_klienta, status, tytul, priorytet, data, godzina, tresc } =
-    req.body;
+const addRaport = async (req, res) => {
+  try {
+    const { id_klienta, status, tytul, priorytet, data, godzina, tresc } =
+      req.body;
 
-  const sqlTicket = `
-      INSERT INTO ticket022025 (id_klienta, status, tytul, priorytet, data, godzina) 
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = (today.getMonth() + 1).toString().padStart(2, "0");
+
+    const ticketTable = `ticket${month}${year}`;
+    const messageTable = `message${month}${year}`;
+
+    const sqlTicket = `
+      INSERT INTO ${ticketTable} (id_klienta, status, tytul, priorytet, data, godzina) 
       VALUES (?, ?, ?, ?, ?, ?)
     `;
 
-  const valuesTicket = [id_klienta, status, tytul, priorytet, data, godzina];
+    const valuesTicket = [id_klienta, status, tytul, priorytet, data, godzina];
 
-  db.query(sqlTicket, valuesTicket, (err, result) => {
-    if (err) {
-      console.error("Error with addRaport:", err);
-      return res
-        .status(500)
-        .json({ success: false, message: `DB error: ${err}` });
-    }
-
-    const ticketId = result.insertId;
+    const [ticketResult] = await db.promise().query(sqlTicket, valuesTicket);
+    const ticketId = ticketResult.insertId;
 
     const sqlMessage = `
-        INSERT INTO message022025 (id_ticket, id_klienta, data, godzina, tresc) 
-        VALUES (?, ?, ?, ?, ?)
-      `;
+      INSERT INTO ${messageTable} (id_ticket, id_klienta, data, godzina, tresc) 
+      VALUES (?, ?, ?, ?, ?)
+    `;
 
     const valuesMessage = [ticketId, id_klienta, data, godzina, tresc];
 
-    db.query(sqlMessage, valuesMessage, (err, resultMessage) => {
-      if (err) {
-        console.error("Error inserting message:", err);
-        return res
-          .status(500)
-          .json({ success: false, message: `DB error: ${err}` });
-      }
+    const [messageResult] = await db.promise().query(sqlMessage, valuesMessage);
 
-      res.status(200).json({
-        success: true,
-        message: "Raport and message added successfully",
-        ticketId: ticketId,
-        messageId: resultMessage.insertId,
-      });
+    res.status(200).json({
+      success: true,
+      message: "Raport and message added successfully",
+      ticketId: ticketId,
+      messageId: messageResult.insertId,
     });
-  });
+  } catch (err) {
+    console.error("Error in addRaport:", err);
+    res
+      .status(500)
+      .json({ success: false, message: `DB error: ${err.message}` });
+  }
 };
 
 const getAllTicketsByClientId = (req, res) => {
