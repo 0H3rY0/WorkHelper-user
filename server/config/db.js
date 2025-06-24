6439b840c73782941814219c1312c5f8432eee75
Szymon <szymers12345@gmail.com>
2025-06-24 22:50:58 +0200

    change db connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,39 +1,35 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-// Tworzenie puli połączeń
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10, // Maksymalna liczba połączeń
+  connectionLimit: 10,
   queueLimit: 0,
 });
 
-// Sprawdzanie połączenia
 db.getConnection((err, connection) => {
   if (err) {
-    console.error("❌ Błąd połączenia z bazą danych:", err.message);
+    console.error("❌ Error with connect db", err.message);
     return;
   }
-  console.log("✅ Połączono z bazą danych MySQL");
-  connection.release(); // Zwolnienie połączenia do puli
+  console.log("✅ successfuly connect tdb");
+  connection.release();
 });
 
-// Pingowanie bazy co 60 sekund, aby uniknąć zamknięcia połączenia
-setInterval(() => {
-  db.getConnection((err, connection) => {
-    if (err) {
-      console.error("⚠️ Ping error:", err.message);
-    } else {
-      console.log("🔄 Ping do bazy danych OK");
-      connection.ping();
-      connection.release();
-    }
-  });
-}, 60000); // Co 60 sekund
+// setInterval(() => {
+//   db.getConnection((err, connection) => {
+//     if (err) {
+//       console.error("⚠️ Ping error:", err.message);
+//     } else {
+//       console.log("🔄 Ping do bazy danych OK");
+//       connection.ping();
+//       connection.release();
+//     }
+//   });
+// }, 60000);
 
-// Eksportowanie `db`
 module.exports = db;
